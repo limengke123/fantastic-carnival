bd2b90627caf2fcc9a2c092a608176cf0e396b4b
limengke <752140235@qq.com>
2018-01-09 23:08:11 +0800

    引入babel 处理类无法设置静态属性

--- server/register.js
+++ server/register.js
@@ -1,3 +1,13 @@
 /**
  * Created by li on 2018/1/9 17:38.
  */
+require('babel-core/register')({
+    'presets':[
+        ['env',{
+            'targets':{
+                'node':'current'
+            }
+        }]
+    ]
+})
+require('./index.js')
