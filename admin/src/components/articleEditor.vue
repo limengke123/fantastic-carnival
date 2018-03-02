8971330f37826f9fa413ec44083a853f53804720
limengke <752140235@qq.com>
2018-03-02 17:33:38 +0800

    articleEditor补充中....

--- admin/src/components/articleEditor.vue
+++ admin/src/components/articleEditor.vue
@@ -1,15 +1,17 @@
 <template lang="pug">
     section
-        div.title-active: input.form-control.big.only-border-bottom(type="text")
+        div: input.form-control.big.only-border-bottom(type="text")
         .header-wrapper
-            .first-container
+            .half-container
                 i.fa.fa-tags(style="margin-right:5px;")
-                span.tag: i.delete-tag.fa.fa-close
+                span.tag(v-for="tag in tags")
+                    | {{tag['name']}}
+                    i.delete-tag.fa.fa-close
                 .tag.active
                     span +
                     input.tag-input(type="text",placeholder="使用回车键提交")
-                    ul.search-list.reset-list: li.search-item(v-for="tag in tagsToAdd") {{tag['name']}}
-            div
+                    ul.search-list.reset-list: li.search-item(v-for="tag in tags") {{tag['name']}}
+            .half-container.btns
                 button.btn.btn-save.r 发布文章
                 button.btn.btn-border.r 删除草稿
         textarea#editor(style="opacity:1")
@@ -25,6 +27,13 @@
                   {
                       name:"一条tag"
                   }
+              ],
+              tags:[
+                  {
+                      name:"tags!!!"
+                  },{
+                      name:"tags222"
+                  },
               ]
           }
         },
@@ -49,19 +58,31 @@
     .title-active
         .big
             border 1px solid $yellow
+    .form-control
+        color $black
+        box-sizing border-box
+        padding 10px 8px
+        width 100%
+        height auto
+        box-shadow none
+        border 1px solid $border
+        background-color #fff
+        outline 0
     .big
         transition border 0.5s
         padding 13px 20px 13px 30px
+        font-size 26px
     .only-border-bottom
         border 1px solid transparent
         border-bottom 1px solid $border
     .header-wrapper
         display flex
-        justify-content space-between
+        //justify-content space-between
         padding 15px
         box-sizing border-box
-    .first-container
+    .half-container
         display flex
+        flex-grow 1
         .tag
             padding 3px 0
             font-size 14px
@@ -76,7 +97,9 @@
                 border-bottom 2px solid $green
                 .delete-tag
                     display inline
-
+    .btns
+        display flex
+        justify-content flex-end
 
 
 </style>
