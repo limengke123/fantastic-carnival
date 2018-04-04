9ee8241e528170533a9fcc32b14ae27022a59883
limengke <752140235@qq.com>
2018-04-04 17:26:03 +0800

    server的总结文章

--- admin/src/router/index.js
+++ admin/src/router/index.js
@@ -16,6 +16,23 @@ const posts = () => import(/* webpackChunkName: "posts" */ '../page/posts.vue')
 const tags = () => import(/* webpackChunkName: "tags" */ '../page/tags.vue')
 const noFound = () => import(/* webpackChunkName: "no-found" */ '../page/noFound.vue')
 
+/**
+ * 如下是官网提供的异步组件的完整写法
+ * */
+
+// const AsyncComp = () => ({
+//     // 需要加载的组件。应当是一个 Promise
+//     component: import('./MyComp.vue'),
+//     // 加载中应当渲染的组件
+//     loading: LoadingComp,
+//     // 出错时渲染的组件
+//     error: ErrorComp,
+//     // 渲染加载中组件前的等待时间。默认：200ms。
+//     delay: 200,
+//     // 最长等待时间。超出此时间则渲染错误组件。默认：Infinity
+//     timeout: 3000
+// })
+
 //注入
 Vue.use(Router)
 
