60c28924508a99621d83f971d13fb2460e6f081e
limengke <752140235@qq.com>
2018-04-13 17:36:51 +0800

    解决切换路由 组件内部还在调用setState情况

--- client/build/webpack.base.config.js
+++ client/build/webpack.base.config.js
@@ -44,7 +44,7 @@ module.exports = {
             template:join(__dirname,'../src/template.html'),
             filename:'index.html',
             inject:'true',
-            favicon:join(__dirname,'../src/assets/img/fav.ico'),
+            favicon:join(__dirname,'../src/assets/img/new_favicon.ico'),
             minify:{
                 removeCommnets:true,
                 collapseWhitespace:true,
